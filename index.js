962f9755b19151f5b72c4e855a3546d6b58dc7d2
JF1HNL <1118040@ed.tus.ac.jp>
2019-03-24 21:57:30 +0900

    button create

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const button_5 = [1, 2, 3, 4, 5];
+const button_6 = [1, 2, 3, 4, 5, 6];
+const button_10 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let Field = new Vue({
 	el: "#robokon",
 	computed: {
@@ -5,9 +8,18 @@ let Field = new Vue({
 			let judge = false;
 			if(this.button.toki_increase.flag + this.button.toki_decrease.flag > 10){
 				judge = true;
-				console.error("ドンデンポールを通過した朱鷺の数が10を超えています。")
+				console.error("toki_error!!!!!")
 			}
 			return judge;
+		},
+		button_judge: function(){
+			let ret = false;
+			for(let i in this.button){
+				if(this.button[i].flag){
+					ret = this.button[i].flag;
+				}
+			}
+			return ret;
 		},
 		tensu: function(){
 			let tensu = { all : 0 };
@@ -19,7 +31,7 @@ let Field = new Vue({
 		}
 	},
 	data: {
-		button : {
+		button: {
 			jiritsu : { value : 50, flag : false },
 			kaisanbutsu : { value : 10, flag : 0 },
 			warigusuri_hakobi : { value : 20, flag : false },
@@ -32,12 +44,22 @@ let Field = new Vue({
 			sansyakudama_complete : { value : 50, flag : false },
 			toki_increase : { value : 30, flag : 0 },
 			toki_decrease : { value : -50, flag : 0 }
-		}
+		},
+		button_5 : button_5,
+		button_6 : button_6,
+		button_10 : button_10
 	},
 	methods: {
 		flag: function(ret){
 			ret = !ret;
 			console.log(ret)
+		},
+		zero_number: function(item, flag){
+			let ret = 0;
+			if(flag !== item){
+				ret = item;
+			}
+			return ret;
 		}
 	}
 })
